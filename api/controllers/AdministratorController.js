6de4273f016ca45586b80b1406c6b297c86e8bfd
heroku <ak6688@student.uni-lj.si>
2016-01-17 20:12:38 +0100

    Grade access.

--- api/controllers/AdministratorController.js
+++ api/controllers/AdministratorController.js
@@ -10,15 +10,22 @@ module.exports = {
     res.view();
   },
   grades: function(req,res) {
-    res.view();
+    var name = req.param("name");
+    var lastname = req.param("lastname");
+    var grades;
+    Student.find({where: {name: name, lastname: lastname}, limit: 1}).populate('grades')
+      .exec(function findCB(err, found){
+      if(found.length > 0) {
+        console.log(found[0].grades);
+        //console.log(typeof found);
+        //var student = JSON.parse(found);
+        //console.log("test");
+        //console.log(student);
+        res.view(found[0].grades);
+      }
+    });
   },
   submitGrade: function(req,res) {
-    res.view();
-  },
-  reports: function(req,res) {
-    res.view();
-  },
-  submitReport: function(req,res) {
     res.view();
   }
 };
